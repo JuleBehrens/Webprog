07f2eac1d95c0dd52dfcf1e1be3abaf3a7627e09
schneiderje <jensxs@gmx.de>
2019-10-25 14:58:50 +0200

    JS Update

--- src/page-standorte/page-standorte.js
+++ src/page-standorte/page-standorte.js
@@ -15,9 +15,27 @@ class PageStandorte {
     /**
      * Seite anzeigen. Wird von der App-Klasse aufgerufen.
      */
-    async show() {
-        // TODO: Seite anzeigen
-    }
+     async show(matches) {
+         let html = await fetch("page-standorte/page-standorte.html");
+         let css = await fetch("page-standorte/page-standorte.css");
+
+         if (html.ok && css.ok) {
+             html = await html.text();
+             css = await css.text();
+         } else {
+             console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+             return;
+         }
+
+         // Seite zur Anzeige bringen
+         let pageDom = document.createElement("div");
+         pageDom.innerHTML = html;
+
+
+         this._app.setPageCss(css);
+
+         this._app.setPageContent(pageDom.querySelector("main"));
+     }
 
 /**
  standortlisteFunction() {
