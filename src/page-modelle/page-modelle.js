c97e5ed4087714d579798a97091d422ca1fd4fa5
JonahSchierding <jonah.schierding@live.de>
2019-11-01 00:51:50 +0100

    Version 1 of page-modelle

--- src/page-modelle/page-modelle.js
+++ src/page-modelle/page-modelle.js
@@ -27,22 +27,43 @@ class PageModelle {
             console.error("Fehler beim Laden des HTML/CSS-Inhalts");
             return;
         }
-        let standor = await database.selectallstandorte();
-        let ol = document.createElement("ol");
-        standor.forEach(s => {
-            let li=document.createElement("li");
-            let text = document.createTextNode(s.PLZ);
-            li.appendChild(text);
-            ol.appendChild(li);
-        });
-        html = html.replace(/{STAPELLAUF}/g, ol.innerHTML);
+
+        //let modelleListe = await database.selectallmodelle();
+        //let ol = document.createElement("ol");
+        //modelleListe.forEach(s => {
+            //let li=document.createElement("li");
+            //let text = document.createTextNode(s.PLZ);
+            //li.appendChild(text);
+            //ol.appendChild(li);
+    //    });
+        //html = html.replace(/{Modelle}/g, ol.innerHTML);
         // Seite zur Anzeige bringen
+
+
         let pageDom = document.createElement("div");
         pageDom.innerHTML = html;
 
+        let mainElement = pageDom.querySelector("#h");
+        let templateElement = pageDom.querySelector("#blank");
 
+        let hallo = document.createElement("div");
+
+        let modelleListe = await database.selectallmodelle();
+        modelleListe.forEach(e => {
+            let html = templateElement.innerHTML;
+            html = html.replace("{SRC}", e.picsrc);
+            html = html.replace("{Name}", e.marke +" "+ e.name);
+            html = html.replace("{Baujahr}", e.baujahr);
+            html = html.replace("{Farbe}", e.farbe);
+            html = html.replace("{PS}", e.ps);
+            html = html.replace("{Preis}", e.tagespreis);
+            html = html.replace("{Automatik}", e.automatik);
+
+            hallo.innerHTML += html;
+        });
+        mainElement.innerHTML = mainElement.innerHTML.replace("{Modelle}",hallo.innerHTML);
+        pageDom.querySelector("main").innerHTML += mainElement.innerHTML;
         this._app.setPageCss(css);
-
         this._app.setPageContent(pageDom.querySelector("main"));
     }
 }
