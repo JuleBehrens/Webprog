cd56bedddcd2a0cdc57b653fe8c121df9220361a
Jule Behrens <julebehrens@Jules-MacBook-Air.local>
2019-11-15 18:58:45 +0100

    carsharing update

--- src/page-carsharing/page-carsharing.js
+++ src/page-carsharing/page-carsharing.js
@@ -29,11 +29,42 @@ class PageCarsharing {
 
          // Seite zur Anzeige bringen
          let pageDom = document.createElement("div");
+         html = html.replace("{tabelle}","");
          pageDom.innerHTML = html;
 
 
          this._app.setPageCss(css);
 
          this._app.setPageContent(pageDom.querySelector("main"));
+         document.querySelector("#suchknopf").addEventListener("click", function(event){
+           _suchen();
+         });
+
      }
+}
+async function _suchen() {
+  let html = await fetch("page-carsharing/page-carsharing.html");
+  let css = await fetch("page-carsharing/page-carsharing.css");
+
+  if (html.ok && css.ok) {
+      html = await html.text();
+      css = await css.text();
+  } else {
+      console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+      return;
+  }
+
+  // Seite zur Anzeige bringen
+  let pageDom = document.createElement("div");
+  html = html.replace("{tabelle}","penis");
+  pageDom.innerHTML = html;
+
+
+  this._app.setPageCss(css);
+
+  this._app.setPageContent(pageDom.querySelector("main"));
+  document.querySelector("#suchknopf").addEventListener("click", function(event){
+    _suchen();
+  });
+
 }
